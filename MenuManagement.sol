ab74c21269487689982870b4825bd77def0496b3
Haaris <124563559+haaris014@users.noreply.github.com>
2024-01-02 02:22:41 +0500

    Updated MenuManagement.sol

--- MenuManagement.sol
+++ MenuManagement.sol
@@ -2,23 +2,118 @@
 pragma solidity ^0.8.0;
 
 contract MenuManagement {
+    address public owner;
+
     struct MenuItem {
+        string name;
         uint256 price;
-        bool available;
+        uint256 available;
     }
 
-    mapping(string => MenuItem) public menu;
+    mapping(uint256 => MenuItem) public menu;
+    uint256 public totalItems;
 
-    function addMenuItem(string memory itemName, uint256 price) external {
-        menu[itemName] = MenuItem(price, true);
+    // Separate array to store menu item keys
+    uint256[] public menuItemIds;
+
+    event MenuItemAdded(
+        uint256 itemId,
+        string itemName,
+        uint256 price,
+        uint256 availability
+    );
+    event MenuItemPriceUpdated(
+        uint256 itemId,
+        string itemName,
+        uint256 newPrice
+    );
+    event MenuItemAvailabilityUpdated(
+        uint256 itemId,
+        string itemName,
+        uint256 newAvailability
+    );
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not the owner");
+        _;
     }
 
-    function updatePrice(string memory itemName, uint256 newPrice) external {
-        require(menu[itemName].available, "Item not found");
-        menu[itemName].price = newPrice;
+    constructor() {
+        owner = msg.sender;
     }
 
-    function checkAvailability(string memory itemName) external view returns (bool) {
-        return menu[itemName].available;
+    function addMenuItem(
+        uint256 itemId,
+        string memory itemName,
+        uint256 price,
+        uint256 available
+    ) external onlyOwner {
+        totalItems++;
+        menu[itemId] = MenuItem(itemName, price, available);
+        menuItemIds.push(itemId);
+        emit MenuItemAdded(itemId, itemName, price, available);
+    }
+
+    function updatePrice(
+        uint256 itemId,
+        string memory itemName,
+        uint256 newPrice
+    ) external onlyOwner {
+        require(menu[itemId].available > 0, "Item not found");
+        menu[itemId].price = newPrice;
+        emit MenuItemPriceUpdated(itemId, itemName, newPrice);
+    }
+
+    function updateAvailability(
+        uint256 itemId,
+        string memory itemName,
+        uint256 newAvailability
+    ) external onlyOwner {
+        require(menu[itemId].available > 0, "Item not found");
+        menu[itemId].available = newAvailability;
+        emit MenuItemAvailabilityUpdated(itemId, itemName, newAvailability);
+    }
+
+    function checkAvailability(uint256 itemId) external view returns (bool) {
+        bool availabe = false;
+        if (menu[itemId].available > 0) {
+            availabe = true;
+            return availabe;
+        }
+        return availabe;
+    }
+
+    function viewMenuItem(
+        uint256 itemId
+    ) public view returns (string memory, uint256, uint256) {
+        // View details of a specific menu item
+        return (menu[itemId].name, menu[itemId].price, menu[itemId].available);
+    }
+
+    function viewMenu()
+        public
+        view
+        returns (
+            uint256[] memory,
+            string[] memory,
+            uint256[] memory,
+            uint256[] memory
+        )
+    {
+        // View the entire menu
+        uint256[] memory itemIds = new uint256[](menuItemIds.length);
+        string[] memory itemNames = new string[](menuItemIds.length);
+        uint256[] memory itemPrices = new uint256[](menuItemIds.length);
+        uint256[] memory itemAvailabilities = new uint256[](menuItemIds.length);
+
+        for (uint256 i = 0; i < menuItemIds.length; i++) {
+            uint256 itemId = menuItemIds[i];
+            itemIds[i] = itemId;
+            itemNames[i] = menu[itemId].name;
+            itemPrices[i] = menu[itemId].price;
+            itemAvailabilities[i] = menu[itemId].available;
+        }
+
+        return (itemIds, itemNames, itemPrices, itemAvailabilities);
     }
 }
