70d87fff155f35d118d0ad05e03f74261eb950a7
Haaris <124563559+haaris014@users.noreply.github.com>
2024-01-02 02:25:00 +0500

    Updated RewardsAndLoyalty.sol

--- RewardsAndLoyalty.sol
+++ RewardsAndLoyalty.sol
@@ -4,13 +4,23 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract RewardsAndLoyalty {
+    address public owner;
     IERC20 public tokenContract;
 
-    constructor(IERC20 _tokenContract) {
-        tokenContract = _tokenContract;
+    event LoyaltyTokensCredited(address indexed user, uint256 amount);
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not the owner");
+        _;
     }
 
-    function creditLoyaltyTokens(address user, uint256 amount) external {
+    constructor(address _tokenContract) {
+        owner = msg.sender;
+        tokenContract = IERC20(_tokenContract);
+    }
+
+    function creditLoyaltyTokens(address user, uint256 amount) external onlyOwner{
         tokenContract.transfer(user, amount);
+        emit LoyaltyTokensCredited(user, amount);
     }
 }
