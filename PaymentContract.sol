19b8a81c33a05a35ec4a7c2c392b11db729090f3
Haaris <124563559+haaris014@users.noreply.github.com>
2024-01-02 02:23:51 +0500

    Updated PaymentContract.sol

--- PaymentContract.sol
+++ PaymentContract.sol
@@ -4,9 +4,21 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract PaymentContract is ERC20 {
-    constructor() ERC20("FastCoin", "FC") {}
+    address public owner;
 
-    function processPayment(address recipient, uint256 amount) external {
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not the owner");
+        _;
+    }
+
+    constructor() ERC20("FastCoin", "FC") {
+        owner = msg.sender;
+    }
+
+    function processPayment(
+        address recipient,
+        uint256 amount
+    ) external onlyOwner {
         _transfer(msg.sender, recipient, amount);
     }
 }
