ca670bf01704a4f1e39a9a69647f33e598f51d0f
Haaris <124563559+haaris014@users.noreply.github.com>
2024-01-02 02:24:18 +0500

    Updated PromotionsAndDiscounts.sol

--- PromotionsAndDiscounts.sol
+++ PromotionsAndDiscounts.sol
@@ -2,14 +2,38 @@
 pragma solidity ^0.8.0;
 
 contract PromotionsAndDiscounts {
+    address public owner;
     mapping(address => uint256) public userDiscounts;
+    //OrderProcessingContract public orderContract;
 
-    function applyPromotion(address user, uint256 discountPercentage) external {
-        require(discountPercentage <= 100, "Invalid discount percentage");
-        userDiscounts[user] = discountPercentage;
+    event PromotionApplied(address indexed user, uint256 discountPercentage);
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not the owner");
+        _;
     }
 
-    function calculateDiscountedAmount(uint256 originalAmount, address user) external view returns (uint256) {
+    constructor() /*address _orderContractAddress*/ {
+        owner = msg.sender;
+        //orderContract = OrderProcessingContract(_orderContractAddress);
+    }
+
+    function applyPromotion(
+        address user,
+        uint256 discountPercentage
+    ) external onlyOwner {
+        require(discountPercentage < 100, "Invalid discount percentage");
+        require(discountPercentage > 0, "Invalid discount percentage");
+        userDiscounts[user] = discountPercentage;
+
+        //uint256 discountAmount = calculateDiscount(orderAmount);
+        emit PromotionApplied(user, discountPercentage);
+    }
+
+    function calculateDiscountedAmount(
+        uint256 originalAmount,
+        address user
+    ) external view returns (uint256) {
         uint256 discountPercentage = userDiscounts[user];
         return (originalAmount * (100 - discountPercentage)) / 100;
     }
