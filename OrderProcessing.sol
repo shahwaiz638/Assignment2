fe34af9172deb67e2c95f4c67437e6f30cb47693
Haaris <124563559+haaris014@users.noreply.github.com>
2024-01-02 02:23:26 +0500

    Updated OrderProcessing.sol

--- OrderProcessing.sol
+++ OrderProcessing.sol
@@ -2,21 +2,39 @@
 pragma solidity ^0.8.0;
 
 contract OrderProcessing {
+    address public owner;
     MenuManagement public menuContract;
 
-    constructor(MenuManagement _menuContract) {
-        menuContract = _menuContract;
+    event OrderPlaced(
+        address indexed user,
+        uint256[] itemIds,
+        uint256[] quantities,
+        uint256 totalAmount
+    );
+
+    constructor(address _menuContract) {
+        owner = msg.sender;
+        menuContract = MenuManagment(_menuContract);
     }
 
-    function placeOrder(string[] memory items, uint256[] memory quantities) external view returns (uint256) {
-        require(items.length == quantities.length, "Invalid input lengths");
+    function placeOrder(
+        uint256[] memory itemId,
+        uint256[] memory quantities
+    ) external view returns (uint256) {
+        require(itemId.length == quantities.length, "Invalid input lengths");
 
         uint256 totalAmount = 0;
 
-        for (uint256 i = 0; i < items.length; i++) {
-            require(menuContract.checkAvailability(items[i]), "Item not available");
-            totalAmount += menuContract.menu(items[i]).price * quantities[i];
+        for (uint256 i = 0; i < itemId.length; i++) {
+            require(
+                menuContract.checkAvailability(itemId[i]),
+                "Item not available"
+            );
+            totalAmount += menuContract.menu(itemId[i]).price * quantities[i];
+            menuContract.menu(itemId[i]).availabe -= quantities[i];
         }
+
+        emit OrderPlaced(msg.sender, itemIds, quantities, totalAmount);
 
         return totalAmount;
     }
