5ea23ab3d2d4d115827bfe5b45b491183f338141
Haaris <124563559+haaris014@users.noreply.github.com>
2024-01-03 01:06:51 +0500

    Updated PaymentContract.sol

--- PaymentContract.sol
+++ PaymentContract.sol
@@ -4,21 +4,50 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract PaymentContract is ERC20 {
-    address public owner;
+    address public owner; // Admin
+    address public cafe;
+    PromotionsAndDiscountsContract public PromotionContract;
 
     modifier onlyOwner() {
         require(msg.sender == owner, "Not the owner");
         _;
     }
 
-    constructor() ERC20("FastCoin", "FC") {
-        owner = msg.sender;
+    modifier onlyCafe() {
+        require(msg.sender == cafe, "Not the cafe");
+        _;
     }
 
-    function processPayment(
-        address recipient,
-        uint256 amount
-    ) external onlyOwner {
-        _transfer(msg.sender, recipient, amount);
+    constructor(address _PromotionContractAddress) ERC20("FastCoin", "FC") {
+        owner = msg.sender;
+        _mint(owner, 1000000 * 10 ** decimals()); // Mint initial supply
+        PromotionContract = PromotionsAndDiscountsContract(
+            _PromotionContractAddress
+        );
+    }
+
+    function setCafe(address _cafe) external onlyOwner {
+        cafe = _cafe;
+    }
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(uint amount) external {
+        _burn(msg.sender, amount);
+    }
+
+    function processPayment(uint256 amount) external payable {
+        require(
+            amount > PromotionContract.getDiscountAmount(msg.sender),
+            "Not enough amount sent"
+        );
+        require(balanceOf(msg.value) > amount, "Insufficent funds in account");
+        _transfer(msg.sender, cafe, amount);
+    }
+
+    function refundPayment(address user, unit256 amount) external onlyCafe {
+        _transfer(cafe, user, amount);
     }
 }
