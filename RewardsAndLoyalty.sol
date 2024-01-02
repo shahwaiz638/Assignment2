3621ac57c14c33967ebab6c795e78405966b84e1
Haaris <124563559+haaris014@users.noreply.github.com>
2024-01-03 01:07:49 +0500

    Updated RewardsAndLoyalty.sol

--- RewardsAndLoyalty.sol
+++ RewardsAndLoyalty.sol
@@ -1,26 +1,41 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-
 contract RewardsAndLoyalty {
     address public owner;
-    IERC20 public tokenContract;
+    mapping(address => uint256) LoyaltyPoints;
+    uint256 PointsPerTransaction = 10;
+    OrderProcessingContract public orderContract;
 
     event LoyaltyTokensCredited(address indexed user, uint256 amount);
+    event LoyaltyPointsUsed(address indexed user, uint256 amount);
 
     modifier onlyOwner() {
         require(msg.sender == owner, "Not the owner");
         _;
     }
 
-    constructor(address _tokenContract) {
+    constructor(address _orderContractAddress) {
         owner = msg.sender;
-        tokenContract = IERC20(_tokenContract);
+        orderContract = OrderProcessingContract(_orderContractAddress);
     }
 
-    function creditLoyaltyTokens(address user, uint256 amount) external onlyOwner{
-        tokenContract.transfer(user, amount);
-        emit LoyaltyTokensCredited(user, amount);
+    function creditLoyaltyTokens(address user) external onlyOwner {
+        require(orderContract.getTotalPrice(user) > 0, "No Transaction Value");
+        uint256 orderAmount = orderContract.getTotalPrice(user);
+        uint256 points = orderAmount * PointsPerTransaction;
+        LoyaltyPoints[user] += points;
+        //tokenContract.transfer(user, amount);
+        emit LoyaltyTokensCredited(user, orderAmount);
+    }
+
+    function ExtractPoints(address user, uint256 points) external onlyOwner {
+        require(LoyaltyPoints[user] >= points, "Not enough loyalty points");
+        LoyaltyPoints[user] -= points;
+        emit LoyaltyPointsUsed(user, points);
+    }
+
+    function getPoints(address user) public view returns (uint256) {
+        return LoyaltyPoints[user];
     }
 }
